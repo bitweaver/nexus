c33f25db0479612e3a04d41c59ae7577276311df
Christian Fowler <spider@viovio.com>
2005-06-29 05:43:38 +0000

    Merge recent changes from R1 into HEAD

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author   xing <xing@synapse.plus.com>
- * @version  $Revision: 1.2 $
+ * @version  $Revision: 1.3 $
  * @package  Nexus
  * @subpackage functions
  */
@@ -11,6 +11,11 @@ $gBitSystem->registerPackage( 'nexus', dirname( __FILE__).'/' );
 if( $gBitSystem->isPackageActive( 'nexus' ) ) {
 	if( $gBitUser->isAdmin() ) {
 			$gBitSystem->registerAppMenu( 'nexus', 'Nexus', NEXUS_PKG_URL.'index.php', 'bitpackage:nexus/menu_nexus.tpl', 'Nexus menus');
+	}
+
+	// check if there is a js file for MSIE
+	if( is_dir( TEMP_PKG_PATH.'nexus/modules/' ) && is_file( TEMP_PKG_PATH.'nexus/modules/hoverfix_array.js' ) ) {
+		$smarty->assign( 'hoverfix', TEMP_PKG_PATH.'nexus/modules/hoverfix_array.js' );
 	}
 }
 ?>
