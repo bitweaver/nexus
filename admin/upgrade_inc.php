c723c984c1871c32a591eb97cb4346582b953ba6
Max Kremmel <xing@synapse.plus.com>
2006-02-01 12:07:58 +0000

    add de-tikification process to upgrade - untested

---
+++ admin/upgrade_inc.php
@@ -0,0 +1,25 @@
+<?php
+
+global $gBitSystem, $gUpgradeFrom, $gUpgradeTo;
+
+$upgrades = array(
+
+	'BWR1' => array(
+		'BWR2' => array(
+// de-tikify tables
+array( 'DATADICT' => array(
+	array( 'RENAMETABLE' => array(
+		'tiki_nexus_plugins' => 'nexus_plugins',
+		'tiki_nexus_menus' => 'nexus_menus',
+		'tiki_nexus_menu_items' => 'nexus_menu_items',
+	)),
+)),
+
+		)
+	),
+);
+
+if( isset( $upgrades[$gUpgradeFrom][$gUpgradeTo] ) ) {
+	$gBitSystem->registerUpgrade( NEXUS_PKG_NAME, $upgrades[$gUpgradeFrom][$gUpgradeTo] );
+}
+?>
